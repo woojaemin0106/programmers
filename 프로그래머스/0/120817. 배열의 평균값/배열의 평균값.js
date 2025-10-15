4a6bd94a287c1b06144b88bddd870136f23b8709
Min02 <dnwoals19@naver.com>
2025-10-15 20:40:00 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    let answer = 0;
+    let qwe = 0;
+        for(let i = 0; i < numbers.length; i++){
+        qwe += numbers[i];
+        }
+    return answer = qwe/numbers.length;
+}
